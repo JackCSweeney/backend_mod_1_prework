45dbe4442a1165d13cb776bc1d1067bad5de5055
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-06 10:50:12 -0800

    Add code to person.rb to complete exercise

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,25 @@
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+    attr_accessor 
+    def initialize(name, weight, height)
+        @name = name
+        @weight = weight
+        @height = height 
+    end
+    def lose_weight(num)
+        @weight -= num
+        puts "#{@name} lost #{num} lbs and now weighs #{@weight} lbs"
+    end
+    def get_taller(num)
+        @height += num
+        puts "#{@name} grew #{num} inches and is now #{@height} inches tall"
+    end
+end
+
+jack = Person.new("Jack", 173, 85)
+jack.lose_weight(3)
+jack.get_taller(4)
+
+
