71a5433f3cdb37ce506ecc75b9763922c67cb331
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-06 10:40:19 -0800

    Add code to burrito.rb to complete exercise

--- day_6/exercises/burrito.rb
+++ day_6/exercises/burrito.rb
@@ -11,9 +11,27 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(new_topping)
+    @toppings = @toppings.push("#{new_topping}")
+  end
+
+  def remove_topping(new_topping)
+    @topping = @toppings.delete("#{new_topping}")
+  end
+  
+  def change_protein(protein)
+    @protein = protein
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+dinner.add_topping("corn")
+p dinner.toppings
+dinner.remove_topping("salsa")
+p dinner.toppings
+dinner.change_protein("chicken")
+p dinner.protein
